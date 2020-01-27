525e1d04980b7c4f7f6e1dff384145e05bbb1870
merewelch214 <merewelch@gmail.com>
2020-01-26 21:05:45 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(names_array, event_name) {
+    let wishes_array = [];
+    for (let i = 0; i < names_array.length; i++) {
+        wishes_array.push(`Thank you, ${names_array[i]}, for the wonderful ${event_name} gift!`)    
+    }
+    return wishes_array;
+}
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num--);
+    }
+}
